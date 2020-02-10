08b7204cea10f604e41b84722516ea8d3064ea6c
mekaleka <www.no-reply.com>
2020-02-10 00:16:28 -0700

    added more to js file and worked on fixing bugs

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-//Reference to the questionn with getElementById.  
+//Reference to the questionn with getElementById.
 const question = document.getElementById("question");
-//Array of choices with getElementByClassName.
+//Array of options with getElementByClassName.
 const options = Array.from(document.getElementsByClassName("option-text"));
-//variables 
+//variables
 let currentQuestion = {};
 //variable for creating a short delay after selecting answers.
 let acceptingAnswers = true;
@@ -10,60 +10,77 @@ let acceptingAnswers = true;
 let score = 0;
 //variable for a Counter
 let questionCounter = 0;
-//variable for accessing questions. 
+//variable for accessing questions.
 let availableQuestions = [];
 
-//Questions with options for answers and a checker for correct answer. 
+//Questions with options for answers and a checker for correct answer.
 let questions = [
     {
-        question: "Inside which HTML element do we put the JavaScript?",
-        option1: "<script>",
-        option2: "<javascript>",
-        option3: "<js>",
-        option4: "<scripting>",
-        answer: 1
-},
-{
-    question: "???",
-        option1: "answer options1",
-        option2: "answer options2",
-        option3: "answer options3",
-        option4: "answer options4",
-        answer: 3
-},
-{
-    question: "How do you write 'Hello World' in an alert box?",
-        option1: "answer options1",
-        option2: "answer options2",
-        option3: "answer options3",
-        option4: "answer options4",
-        answer: 4
-}
-],
-//A correct bonus for when a question is answered correctly. 
-const CORRECT_BONUS = 10;
-//How many questions the user gets..
-const MAX_QUESTIONS = 3;
-//Start with a counter, score, and a copy of the avail questions array.  
-startGame = () => {
+      question: "Inside which HTML element do we put the JavaScript?",
+      option1: "<script>",
+      option2: "<javascript>",
+      option3: "<js>",
+      option4: "<scripting>",
+      answer: 1
+    },
+    {
+      question:
+        "What is the correct syntax for reerring to an external script called 'xxx.js'?",
+      option1: "<script href='xxx.js'>",
+      option2: "<script name='xxx.js'>",
+      option3: "<script src='xxx.js'>",
+      option4: "<script file='xxx.js'>",
+      answer: 3
+    },
+    {
+      question: "How do you write 'Hello World' in an alert box?",
+      option1: "msgBox('Hello World');",
+      option2: "alertBox('Hello World');",
+      option3: "msg('Hello World');",
+      option4: "alert('Hello World');",
+      answer: 4
+    }
+  ],
+  //A correct bonus for when a question is answered correctly.
+  // const CORRECT_BONUS = 10;
+  //How many questions the user gets..
+  // const MAX_QUESTIONS = 3;
+
+  //Start with a counter, score, and a copy of the avail questions array.
+  startGame = () => {
     questionCounter = 0;
     score = 0;
     availableQuestions = [...questions];
     console.log(availableQuestions);
     getNewQuestion();
-};
-//gets a new random question 
-getNewQuestion = () => {
+  };
 
-    questionCounter++;
-    const questionIndex = Math.floor(Math.random() * availableQuestions.length);
-    currentQuestion = availableQuestions[questionIndex];
-    question.innerText = currentQuestion.question;
-//
-    options.forEach( option => {
-        const number = option.dataset["number"];
-        option.innerText = currentQuestion["option" + number];
-    });
-},
+//gets a new random question
+getNewQuestion = () => {
+  questionCounter++;
+  const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+  currentQuestion = availableQuestions[questionIndex];
+  question.innerText = currentQuestion.question;
+
+  options.forEach(option => {
+    const number = option.dataset["number"];
+    option.innerText = currentQuestion["option" + number];
+  });
+
+  availableQuestions.splice(questionIndex, 1);
+  console.log(availableQuestions);
+  acceptingAnswers = true;
+};
+options.forEach(option => {
+  option.addEventListener("click", e => {
+    if (!acceptingAnswers) return;
+
+    acceptingAnswers = false;
+    const selectedOption = e.target;
+    const selectedAnswer = selectedOption.dataset["number"];
+    console.log(selectedAnswer);
+    getNewQuestion();
+  });
+});
 
 startGame();
