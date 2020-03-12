3948ff341d771fd280c209f95bf14433617f167d
mekaleka <www.no-reply.com>
2020-03-11 20:02:02 -0700

    updated local storage and top scores

--- highScores.js
+++ highScores.js
@@ -1,7 +1,16 @@
 const highScoresList = document.getElementById("highScoresList");
 const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+const score = JSON.parse(localStorage.getItem("mostRecentScore"));
+console.log(highScores);
+console.log("recentScore", score)
+// highScores.innerHTML = highScores
+//   .map(score => {
+//     return `<li class="high-score">${score.name} - ${score.score}</li>`;
+//   })
+//   .join("");
+  var list = "";
+for (let index = 0; index < highScores.length; index++) {
+    list += `<li class="high-score">${highScores[index].score}</li>`
+}
 
-highScores.innerHTML = highScores
-.map(score => {
-    return `<li class="high-score">${score.name} - ${score.score}</li>`;})
-    .join("");
+highScoresList.innerHTML = list
